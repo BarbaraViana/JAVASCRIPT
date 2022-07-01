00e298df8b9935f4b6c1e108ea8e6ce36e3da368
Barbara Viana <81521722+BarbaraViana@users.noreply.github.com>
2022-07-01 20:31:36 -0300

    JavaScript Assincrono
    Promisses/Async/Await
    API/Fetch

---
+++ JavaScript Assincrono/assets/Js/cat_move.js
@@ -0,0 +1,19 @@
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const catBtn = document.getElementById('change-cat');
+
+const getCat = async() => {
+    const data = await fetch(BASE_URL)
+        .then((res) => res.json())
+        .catch((e) => console.log(e));
+    return data.webpurl;
+
+};
+
+const loadImg = async() => {
+    const catImg = document.getElementById('cat');
+    catImg.src = await getCat();
+};
+
+catBtn.addEventListener('click', loadImg);
+
+loadImg();
